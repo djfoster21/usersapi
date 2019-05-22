ca8d2fd57e4063b7ae6fdd24a61bedab943dfb5b
Jason Foster <jason@djfoster.net>
2019-05-22 15:07:06 +0200

    Streamlined responses

--- src/main/java/net/djfoster/api/users/user/UserController.java
+++ src/main/java/net/djfoster/api/users/user/UserController.java
@@ -1,49 +1,87 @@
 package net.djfoster.api.users.user;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.http.ResponseEntity.created;
 
 @RestController
 public class UserController {
     private final UserRepository repository;
 
-    UserController(UserRepository repository) {
+    private final UserResourceAssembler assembler;
+
+    UserController(UserRepository repository, UserResourceAssembler assembler) {
+
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/users")
-    List<User> all() {
-        return repository.findAll();
-    }
+    Resources<Resource<User>> all() {
+        List<Resource<User>> users = repository.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
 
-    @PostMapping("/users")
-    User newUser(@RequestBody User newUser) {
-        return repository.save(newUser);
+        return new Resources<>(users,
+                linkTo(methodOn(UserController.class).all()).withSelfRel());
     }
 
     @GetMapping("/users/{id}")
-    User findUser(@PathVariable Long id) {
-        return repository.findById(id)
+    Resource<User> one(@PathVariable Long id) {
+        User user = repository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
+        return this.assembler.toResource(user);
+    }
+
+    @PostMapping("/users")
+    ResponseEntity<?> newUser(@RequestBody User newUser) throws URISyntaxException {
+        Resource<User> resource = assembler.toResource(repository.save(newUser));
+
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
     }
 
     @PutMapping("/users/{id}")
-    User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
-        return repository.findById(id)
-                .map(user -> {
-                    user.setUsername(newUser.getUsername());
-                    user.setEmail(newUser.getEmail());
-                    return repository.save(user);
-                })
-                .orElseGet(() -> {
-                    newUser.setId(id);
-                    return repository.save(newUser);
-                });
+    ResponseEntity<?> replaceUser(@RequestBody User newUser, @PathVariable Long id) throws URISyntaxException {
+        try{
+            User updatedUser = repository.findById(id)
+                    .map(user -> {
+                        user.setUsername(newUser.getUsername());
+                        user.setEmail(newUser.getEmail());
+                        return repository.save(user);
+                    })
+                    .orElseGet(() -> {
+                        return null;
+                    });
+            Resource<User> resource = assembler.toResource(updatedUser);
+
+            return ResponseEntity
+                    .created(new URI(resource.getId().expand().getHref()))
+                    .body(resource);
+        }catch (NullPointerException e)
+        {
+            throw new UserNotFoundException(id);
+        }
+
+
     }
 
     @DeleteMapping("/users/{id}")
-    void deleteUser(@PathVariable Long id) {
+    ResponseEntity<?> deleteUser(@PathVariable Long id) {
         repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
